a50bebbf0bf7663072ef6c0f3220ebe931beb3f5
davszs <dssantos4409@gmail.com>
2024-08-12 19:57:49 -0300

    quarto execício

---
+++ quarto.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $data = "12 de agosto de 2024";
+ $salario = 850.00;
+ $idade = 18;
+ $cargo = 'Estagiário';
+ $result = true;
+
+ 	echo "Data: $data <br />";
+ 	echo "Salário: $salario <br / >";
+ 	echo "Idade: $idade <br />";
+ 	echo "cargo: $cargo <br />";
+ 	if ($result) {
+ 		echo "Resultado é verdadeiro.";
+ 	}
+ 
+ ?>
